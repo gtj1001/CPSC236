fb51fdc73b8cbf1c155ef8798427b4fcd5dc3677
gtj1001 <157513636+gtj1001@users.noreply.github.com>
2024-04-12 12:12:26 -0400

    Add files via upload

---
+++ Group 10 Final Project/Game 1 - Snake/Snake.py
@@ -0,0 +1,335 @@
+# Wormy (a Nibbles clone)
+# By Al Sweigart al@inventwithpython.com
+# http://inventwithpython.com/pygame
+# Released under a "Simplified BSD" license
+
+import random, pygame, sys
+from pygame.locals import *
+
+FPS = 15
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+CELLSIZE = 20
+assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
+assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
+CELLWIDTH = int(WINDOWWIDTH / CELLSIZE)
+CELLHEIGHT = int(WINDOWHEIGHT / CELLSIZE)
+
+#             R    G    B
+WHITE     = (255, 255, 255)
+BLACK     = (  0,   0,   0)
+RED       = (255,   0,   0)
+GREEN     = (  0, 255,   0)
+DARKGREEN = (  0, 155,   0)
+DARKGRAY  = ( 40,  40,  40)
+LIGHTGRAY = ( 70,  70,  70)
+ORANGE    = (255, 190,   0)
+DARKORANGE= (156, 117,   0)
+YELLOW    = (255, 255,   0)
+BGCOLOR = BLACK
+
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
+
+HEAD = 0 # syntactic sugar: index of the worm's head
+
+def main():
+    global FPSCLOCK, DISPLAYSURF, BASICFONT
+
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+    pygame.display.set_caption('Wormy')
+    initialize_hiscores()
+    showStartScreen()
+    while True:
+        score=runGame()
+        updateHighScore(score)
+        showGameOverScreen(score)
+
+
+def runGame():
+    # Set a random start point.
+    startx = random.randint(5, CELLWIDTH - 6)
+    starty = random.randint(5, CELLHEIGHT - 6)
+    wormCoords = [{'x': startx,     'y': starty},
+                  {'x': startx - 1, 'y': starty},
+                  {'x': startx - 2, 'y': starty}]
+    direction = RIGHT
+
+    # Start the apple in a random place.
+    apple = getRandomLocation()
+    # Set a mine, not inside the apple though.
+    while True:
+        minefield=[]
+        minefield.append(getRandomLocation())
+        if minefield[0]!=apple:
+            break
+
+    while True: # main game loop
+        for event in pygame.event.get(): # event handling loop
+            if event.type == QUIT:
+                terminate()
+            elif event.type == KEYDOWN:
+                if (event.key == K_LEFT or event.key == K_a) and direction != RIGHT:
+                    direction = LEFT
+                elif (event.key == K_RIGHT or event.key == K_d) and direction != LEFT:
+                    direction = RIGHT
+                elif (event.key == K_UP or event.key == K_w) and direction != DOWN:
+                    direction = UP
+                elif (event.key == K_DOWN or event.key == K_s) and direction != UP:
+                    direction = DOWN
+                elif event.key == K_ESCAPE:
+                    terminate()
+
+        # check if the worm has hit itself, amine or the edge
+        if wormCoords[HEAD]['x'] == -1 or wormCoords[HEAD]['x'] == CELLWIDTH or wormCoords[HEAD]['y'] == -1 or wormCoords[HEAD]['y'] == CELLHEIGHT:
+            return (len(wormCoords)-3) # game over
+        for wormBody in wormCoords[1:]:
+            if wormBody['x'] == wormCoords[HEAD]['x'] and wormBody['y'] == wormCoords[HEAD]['y']:
+                return (len(wormCoords)-3) # game over
+        for mineLocation in minefield:
+            if wormCoords[HEAD]['x']==mineLocation['x'] and wormCoords[HEAD]['y']==mineLocation['y']:
+                return (len(wormCoords)-3) # game over
+
+        # check if worm has eaten an apple
+        if wormCoords[HEAD]['x'] == apple['x'] and wormCoords[HEAD]['y'] == apple['y']:
+            # don't remove worm's tail segment
+            while True:
+                apple = getRandomLocation() # set a new apple somewhere
+                if apple not in minefield:  # do not spawn apple in an existing mine
+                    break
+            while True:
+                index=len(minefield)
+                minefield.append(getRandomLocation())
+                if minefield[index-1]!=apple:
+                    break  # set new mine, not inside the apple though
+        else:
+            del wormCoords[-1] # remove worm's tail segment
+
+        # move the worm by adding a segment in the direction it is moving
+        if direction == UP:
+            newHead = {'x': wormCoords[HEAD]['x'], 'y': wormCoords[HEAD]['y'] - 1}
+        elif direction == DOWN:
+            newHead = {'x': wormCoords[HEAD]['x'], 'y': wormCoords[HEAD]['y'] + 1}
+        elif direction == LEFT:
+            newHead = {'x': wormCoords[HEAD]['x'] - 1, 'y': wormCoords[HEAD]['y']}
+        elif direction == RIGHT:
+            newHead = {'x': wormCoords[HEAD]['x'] + 1, 'y': wormCoords[HEAD]['y']}
+        wormCoords.insert(0, newHead)
+        DISPLAYSURF.fill(BGCOLOR)
+        drawGrid()
+        drawWorm(wormCoords)
+        drawApple(apple)
+        for item in minefield:
+            new='f'
+            if minefield.index(item)==(len(minefield)-1):
+                new='t'
+            drawMine(item,new)
+        drawScore(len(wormCoords) - 3)
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+def drawPressKeyMsg():
+    pressKeySurf = BASICFONT.render('Press a key to play.', True, WHITE)
+    pressKeyRect = pressKeySurf.get_rect()
+    pressKeyRect.topleft = (WINDOWWIDTH - 200, WINDOWHEIGHT - 30)
+    DISPLAYSURF.blit(pressKeySurf, pressKeyRect)
+
+
+def checkForKeyPress():
+    if len(pygame.event.get(QUIT)) > 0:
+        terminate()
+
+    keyUpEvents = pygame.event.get(KEYUP)
+    if len(keyUpEvents) == 0:
+        return None
+    if keyUpEvents[0].key == K_ESCAPE:
+        terminate()
+    return keyUpEvents[0].key
+
+
+def showStartScreen():
+    titleFont = pygame.font.Font('freesansbold.ttf', 100)
+    titleSurf1 = titleFont.render('Snake', True, WHITE)
+    titleSurf2 = titleFont.render('Snake', True, ORANGE)
+    titleSurf3 = titleFont.render('Snake', True, DARKORANGE)
+
+    degrees1 = 0
+    scale=1
+    grow="True"
+    while True:
+        DISPLAYSURF.fill(BGCOLOR)
+        rotatedSurf1 = pygame.transform.rotate(titleSurf1, degrees1)
+        rotatedRect1 = rotatedSurf1.get_rect()
+        rotatedRect1.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
+        DISPLAYSURF.blit(rotatedSurf1, rotatedRect1)
+
+        rotatedSurf3 = pygame.transform.scale_by(titleSurf3, 1+((scale-1)/2))
+        rotatedRect3 = rotatedSurf3.get_rect()
+        rotatedRect3.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
+        DISPLAYSURF.blit(rotatedSurf3, rotatedRect3)
+
+        rotatedSurf2 = pygame.transform.scale_by(titleSurf2, scale)
+        rotatedRect2 = rotatedSurf2.get_rect()
+        rotatedRect2.center = (WINDOWWIDTH / 2, WINDOWHEIGHT / 2)
+        DISPLAYSURF.blit(rotatedSurf2, rotatedRect2)
+
+        drawPressKeyMsg()
+
+        if checkForKeyPress():
+            pygame.event.get() # clear event queue
+            return
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+        # Create heartbeat-ish pattern
+        if scale>=1.7 and grow=="True":
+            grow="False"
+        if scale<=1 and grow=="False":
+            grow="True"
+        if grow=="True":
+            scale+=0.1
+        if grow=="False":
+            scale-=0.1
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def getRandomLocation():
+    return {'x': random.randint(0, CELLWIDTH - 1), 'y': random.randint(0, CELLHEIGHT - 1)}
+
+
+def showGameOverScreen(score):
+    # Screen formatting
+    DISPLAYSURF.fill(BGCOLOR)
+    gameOverFont = pygame.font.Font('freesansbold.ttf', 90)
+    hiscoreTitleFont = pygame.font.Font('freesansbold.ttf', 40)
+    hiscoresFont = pygame.font.Font('freesansbold.ttf', 30)
+    hiscores=getHighScores()
+    gameSurf = gameOverFont.render('Game Over', True, WHITE)
+    hiscoreTitleSurf = hiscoreTitleFont.render('High Scores',True,WHITE)
+    gameRect = gameSurf.get_rect()
+    hiscoreTitleRect = hiscoreTitleSurf.get_rect()
+    gameRect.midtop = (WINDOWWIDTH / 2, 10)
+    hiscoreTitleRect.midtop = (WINDOWWIDTH / 2, gameRect.height + 20)
+    DISPLAYSURF.blit(gameSurf, gameRect)
+    DISPLAYSURF.blit(hiscoreTitleSurf, hiscoreTitleRect)
+
+    # Display hiscores as a numbered list
+    scoreIndex=1
+    for item in hiscores:
+        item=item[:-1]
+        # Conditional statement normalizes spacing
+        if scoreIndex<=9:
+            scoreSurf = hiscoresFont.render(str(scoreIndex)+".        "+str(item),True,WHITE)
+        else:
+            scoreSurf = hiscoresFont.render(str(scoreIndex)+".      "+str(item),True,WHITE)            
+        scoreRect = scoreSurf.get_rect()
+        scoreRect.midtop = (WINDOWWIDTH / 2, gameRect.height + 10 + 25 + (scoreIndex*25))
+        DISPLAYSURF.blit(scoreSurf, scoreRect)
+        scoreIndex+=1
+    
+    drawScore(score)
+    drawPressKeyMsg()
+    pygame.display.update()
+    pygame.time.wait(500)
+    checkForKeyPress() # clear out any key presses in the event queue
+
+    while True:
+        if checkForKeyPress():
+            pygame.event.get() # clear event queue
+            return
+
+def getHighScores():
+    # Specifically grabbing hiscores for use by the Game Over screen
+    with open("snake_hiscores.txt","r") as file:
+        hiscores=[]
+        for line in file:
+            hiscores.append(line)
+    return hiscores
+    
+
+def updateHighScore(score):
+    # Read hiscores file into a list
+    with open("snake_hiscores.txt","r") as file:
+        hiscores=[]
+        for line in file:
+            line=line.rstrip()
+            hiscores.append(int(line))
+            # Correct for formatting oddities like trailing \n's
+            while True:
+                if len(hiscores)>10:
+                    hiscores.pop()
+                else:
+                    break
+    # See if new score beats the last entry, replace lowest entry with new score and re-sort.
+    lowest_score=hiscores[9]
+    if int(score)>int(lowest_score):
+        hiscores[9]=int(score)
+        hiscores.sort(reverse=True)
+    #Write that down, write that down!
+    with open("snake_hiscores.txt","w") as file:
+        for item in hiscores:
+            file.write(str(item)+"\n")
+
+
+def drawScore(score):
+    scoreSurf = BASICFONT.render('Score: %s' % (score), True, WHITE)
+    scoreRect = scoreSurf.get_rect()
+    scoreRect.topleft = (WINDOWWIDTH - 120, 10)
+    DISPLAYSURF.blit(scoreSurf, scoreRect)
+
+
+def drawWorm(wormCoords):
+    for coord in wormCoords:
+        x = coord['x'] * CELLSIZE
+        y = coord['y'] * CELLSIZE
+        wormSegmentRect = pygame.Rect(x, y, CELLSIZE, CELLSIZE)
+        pygame.draw.rect(DISPLAYSURF, DARKGREEN, wormSegmentRect)
+        wormInnerSegmentRect = pygame.Rect(x + 4, y + 4, CELLSIZE - 8, CELLSIZE - 8)
+        pygame.draw.rect(DISPLAYSURF, GREEN, wormInnerSegmentRect)
+
+
+def drawApple(coord):
+    x = coord['x'] * CELLSIZE
+    y = coord['y'] * CELLSIZE
+    appleRect = pygame.Rect(x, y, CELLSIZE, CELLSIZE)
+    pygame.draw.rect(DISPLAYSURF, RED, appleRect)
+
+
+def drawMine(coord,new):
+    # Cloned drawApple function
+    x=coord['x'] * CELLSIZE
+    y=coord['y'] * CELLSIZE
+    mineRect = pygame.Rect(x, y, CELLSIZE, CELLSIZE)
+    pygame.draw.rect(DISPLAYSURF, DARKORANGE, mineRect)
+    # Newest mine is highlighted in yellow
+    if new=='t':
+        innerMineRect = pygame.Rect(x+3,y+3,CELLSIZE-6,CELLSIZE-6)
+        pygame.draw.rect(DISPLAYSURF, YELLOW, innerMineRect)
+    else:
+        innerMineRect = pygame.Rect(x+3,y+3,CELLSIZE-6,CELLSIZE-6)
+        pygame.draw.rect(DISPLAYSURF, ORANGE, innerMineRect)
+
+def drawGrid():
+    for x in range(0, WINDOWWIDTH, CELLSIZE): # draw vertical lines
+        pygame.draw.line(DISPLAYSURF, DARKGRAY, (x, 0), (x, WINDOWHEIGHT))
+    for y in range(0, WINDOWHEIGHT, CELLSIZE): # draw horizontal lines
+        pygame.draw.line(DISPLAYSURF, DARKGRAY, (0, y), (WINDOWWIDTH, y))
+
+def initialize_hiscores():
+    # Try to find a hiscores file. If not there, make a new one and fill it with 0s
+    try:
+        x=open("snake_hiscores.txt")
+    except FileNotFoundError:
+        with open("snake_hiscores.txt","w") as file:
+            for i in range(10):
+                file.write("0\n")
+
+if __name__ == '__main__':
+    main()
